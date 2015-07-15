672c1687dd6aae461ac6b9690a67064978ae99fa
Chris Mannel <cmannel77@gmail.com>
2015-07-15 12:28:11 -0500

    Practice updates

---
+++ script.js
@@ -0,0 +1,3 @@
+//alert('AND... Holy Moley! This is the SECOND alert contained in this wonderful linked JS file!');
+
+//document.write ('<h3>This is a new H3 tag I created in JS!</h3>' + '<h4>This is SO dang cool</h4>');
